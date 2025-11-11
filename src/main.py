ac9639e415bd2348fb1348d3905df25f19586c18
Eric García Gómez <garciagomezeric10@gmail.com>
2025-11-11 21:14:11 +0100

    add movies router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from src.routers import movies
+
 app = FastAPI(
     title="Movies_API", description="A simple application for CRUD operations with movies"
 )
+
+app.include_router(movies.router)
 
 
 @app.get("/")
